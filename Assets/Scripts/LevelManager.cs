2cbacf33667cfc4d8b6bfa06ed2c81cb656ca042
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-05-20 15:29:06 +0530

    Splash Screen and Start Screen Completed

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float splashDelay = 5;
+	// Use this for initialization
+	void Start () {
+		Invoke ("LoadNextLevel", splashDelay);
+	}
+
+	public void LoadNextLevel(){
+		UnityEngine.SceneManagement.SceneManager.LoadScene 
+			(UnityEngine.SceneManagement.SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+	public void LoadLevel(string level){
+		UnityEngine.SceneManagement.SceneManager.LoadScene (level);
+	}
+	public void QuitRequest(){
+		Debug.Log ("Quit Requested");
+		Application.Quit ();
+	}
+}
