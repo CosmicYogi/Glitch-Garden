c4f09b7fb763bf274cbbe6dacc198ec150cc0549
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-05-20 17:03:23 +0530

    Music System added

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	private 
+	AudioSource audioSource;
+	public AudioClip[] levelMusicChangeArray;
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Awake(){
+		DontDestroyOnLoad (gameObject);
+		Debug.Log ("Don't destry on load" + name); 
+	}
+
+	void OnLevelWasLoaded(int levelIndex){
+		AudioClip thisLevelMusic = levelMusicChangeArray [levelIndex];
+		Debug.Log ("Playing clip : " + thisLevelMusic);
+
+		if (thisLevelMusic) { //If something is there in thisLevelMusic
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+			//audioSource.GetComponent<AudioSource> ().Play;
+		}
+	}
+}
