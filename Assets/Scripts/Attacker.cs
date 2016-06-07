bbe357eceb413e896f74898a97ecf2c68aff99ef
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-06-07 13:31:01 +0530

    Attackers Movement complete and some overall polishing

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Attacker : MonoBehaviour {
 
 	[Range (-1.5f,2f)] //Add this block above declaration to range it's value.
-	public float walkSPeed;
+	public float currentSPeed;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,6 +12,18 @@ public class Attacker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.Translate (Vector2.left * walkSPeed * Time.deltaTime);
+		this.transform.Translate (Vector2.left * currentSPeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(){
+		print (name + "Trigger Enter");
+	}
+
+	void setCurrentSpeed(float speed){
+		currentSPeed = speed;
+	}
+
+	void strikeCurrentTarget(float damage){
+		print (name + ",damaged amount " + damage);
 	}
 }
