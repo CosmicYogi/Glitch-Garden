f66b6c581ad3d3a58f51318050914f2ede395ede
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-06-07 15:03:53 +0530

    commit, Commit on Attackers Functionality

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(Rigidbody2D))]
 public class Attacker : MonoBehaviour {
 
-	[Range (-1.5f,2f)] //Add this block above declaration to range it's value.
-	public float currentSPeed;
+	//[Range (-1.5f,2f)] //Add this block above declaration to range it's value.
+	//Earlier this currentSpeed variable was public to check what speed would be best, now privatized.
+	private float currentSPeed;
+	private GameObject currentTarget;
 	// Use this for initialization
 	void Start () {
 	
@@ -23,7 +26,11 @@ public class Attacker : MonoBehaviour {
 		currentSPeed = speed;
 	}
 
-	void strikeCurrentTarget(float damage){
+	public void strikeCurrentTarget(float damage){
 		print (name + ",damaged amount " + damage);
+	}
+
+	public void attack(GameObject obj){
+		currentTarget = obj;
 	}
 }
---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Attacker))]
+public class Fox : MonoBehaviour {
+
+	private Attacker attacker;
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		attacker = GetComponent<Attacker> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		GameObject obj = coll.gameObject;
+
+		//Do nothing if not colliding with Defender.
+		if (!obj.GetComponent<Defender> ()) {
+			return;
+		}
+
+		if (obj.GetComponent<Stone> ()) {
+			anim.SetTrigger ("jumpTrigger");
+		} else {
+			anim.SetBool ("isAttacking", true);
+			attacker.attack (obj);
+		}
+		print ("Fox Collided with " + coll);
+	}
+}
