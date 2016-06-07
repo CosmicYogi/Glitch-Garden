ef12c6bc190ebd69605c7edac8259484ba49e989
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-06-07 11:50:59 +0530

    Addded movement to Attackers, Broken Prefabs remade.

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+	[Range (-1.5f,2f)] //Add this block above declaration to range it's value.
+	public float walkSPeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (Vector2.left * walkSPeed * Time.deltaTime);
+	}
+}
