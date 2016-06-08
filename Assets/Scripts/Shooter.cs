a23fa1ec01d4c8e9a6ccdc9caeadd2e974d760b9
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-06-08 16:36:37 +0530

    Defenders and Projectiles

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+	public GameObject projectile;
+	public GameObject projectileParent;
+	public GameObject gun;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void fireGun(){
+		GameObject newProjectile = Instantiate (projectile) as GameObject;
+		newProjectile.transform.parent = projectileParent.transform;
+		newProjectile.transform.position = gun.transform.position;
+	}
+}
