cec46b6f889d1952184a76cee334f03d7abef39e
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-06-15 17:14:31 +0530

    Start count added in front of buttons

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Button : MonoBehaviour {
 
 	private Button[] buttonArray;
+	private Text starCostText;
 	public GameObject defenderPrefaf; //This is made so that an GameObjectPrefabs(OF DEFENDERS) corresponding to each individual button
 	//can be dragged and dropped here for doing operations with GameObject corresponding to the respective Button.
 	public static GameObject selectedDefender;
@@ -11,11 +13,8 @@ public class Button : MonoBehaviour {
 	void Start () {
 		//this.GetComponent<SpriteRenderer> ().color = Color.black;
 		buttonArray = GameObject.FindObjectsOfType<Button> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		starCostText = GetComponentInChildren<Text> ();
+		starCostText.text = "" + defenderPrefaf.GetComponent<Defender> ().starCost;
 	}
 
 	void OnMouseDown(){
