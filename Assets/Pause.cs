cc2c0e40ae6ccdb8317fd1b4ad7a1d2f7db2e6e0
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-06-16 16:46:31 +0530

    Play/Pause functionality implemented

---
+++ Assets/Pause.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	public Sprite pause;
+	public Sprite play;
+	private SpriteRenderer spriteRenderer;
+	void Start(){
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	void OnMouseDown(){
+		if (Time.timeScale != 0) {
+			spriteRenderer.sprite = play;
+			Time.timeScale = 0;
+		} else if (Time.timeScale == 0) {
+			spriteRenderer.sprite = pause;
+			Time.timeScale = 1;
+		}
+	}
+}
