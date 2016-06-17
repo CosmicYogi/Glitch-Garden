769ce8c3ae7398b290d8e959a70a21e70d494824
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-06-17 19:09:53 +0530

    Informaton mask added

---
+++ Assets/Scripts/GameMask.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMask : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		if (GetComponent<CircleCollider2D> ()) {
+			Time.timeScale = 1;
+			Destroy (gameObject);
+		}
+	}
+}
