2a03e506e96455618b9ab6e2c4e471a1dea3ec14
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-06-19 23:14:37 +0530

    Scripts and functionality added to Cock,Pig and Skakrow

---
+++ Assets/Scripts/Piggy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Piggy : MonoBehaviour {
+
+	private Animator animator;
+	private Attacker attacker;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		attacker = GetComponent<Attacker> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void OnTriggerEnter2D(Collider2D coll){
+		GameObject obj = coll.gameObject;
+
+		if (!obj.GetComponent<Defender> ()) {
+			return;
+		} 
+		animator.SetBool ("isAttacking", true);
+		attacker.attack (obj);
+	}		
+}
