032c45714490176cbc1d0b93da82873b6937fbf5
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-06-20 00:19:30 +0530

    Gameplay Polished

---
+++ Assets/Scripts/Chicken.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chicken : MonoBehaviour {
+
+	private Animator animator;
+	private Attacker attacker;
+	// Use this for initialization
+	void Start () {
+		attacker = GetComponent<Attacker> ();
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		GameObject obj = coll.gameObject;
+
+		if (!obj.GetComponent<Defender> ()) {
+			return;
+		}
+		animator.SetBool ("isAttacking", true);
+		attacker.attack (obj);
+	}
+}
